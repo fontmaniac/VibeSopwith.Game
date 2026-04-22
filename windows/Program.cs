8fe087b10b57eaff13f9366a45cf58acd2d24b18
Alexey Kosobrukhov <fontmaniac@users.noreply.github.com>
2026-04-22 17:24:32 +1000

    Separated Windows executable

---
+++ windows/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace VibeSopwith.Game;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("VibeSopwith.Game started");
+
+        using (TheGame game = new TheGame())
+        {
+            game.Run();
+            Console.WriteLine("VibeSopwith.Game exited");
+        }
+    }
+}
